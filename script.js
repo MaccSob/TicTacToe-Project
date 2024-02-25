116862e059cfdf3a508baf1e5a23e02f7c350103
MaccSob <mcdarkdream1@gmail.com>
2024-02-26 00:33:45 +0100

    Making some logic, added players

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const Player = function (name, mark, score) {
+    return {
+    name: name,
+    mark: mark,
+    score: score,
+    }
+};
+let player1 = Player("John", "X", 0);
+let player2 = Player("Daniel", "O", 2);
+
+
+
+
 const gameBoard = [];
